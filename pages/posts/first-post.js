5cff106aeb95fc2934cbb8a69e07eec2ba467f71
Joe Davey <capncrockett@gmail.com>
2022-03-16 11:48:46 -0700

    Prettier Extension added
    Cleaned up some `;`

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,28 +1,25 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import Head from 'next/head'
-
+import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
 
 export default function FirstPost() {
-    return (
+  return (
     <>
-    <Head>
+      <Head>
         <title>First Post</title>
-    </Head>
-    <Image
-    src="/images/profile.jpg" // Route of the image file
-    height={144} //Desried size with correct aspect ratio
-    width={144} //Desried size with correct aspect ratio
-    alt="It's Joe"
-    />
-    <h1>First Post</h1>
-    <h2>
+      </Head>
+      <Image
+        src="/images/profile.jpg" // Route of the image file
+        height={144} //Desried size with correct aspect ratio
+        width={144} //Desried size with correct aspect ratio
+        alt="It's Joe"
+      />
+      <h1>First Post</h1>
+      <h2>
         <Link href="/">
-            <a>Back to home</a>
+          <a>Back to home</a>
         </Link>
-    </h2>
+      </h2>
     </>
-    )
-
+  );
 }
-
