b3dfbe9a7b8acf680ea170bf97a532984d723455
Joe Davey <capncrockett@gmail.com>
2022-03-17 08:15:14 -0700

    Add styling to first-post Add components dir Add _app.js for Global styling

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,25 +1,28 @@
 import Link from "next/link";
 import Image from "next/image";
 import Head from "next/head";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
     <>
-      <Head>
-        <title>First Post</title>
-      </Head>
-      <Image
-        src="/images/profile.jpg" // Route of the image file
-        height={144} //Desried size with correct aspect ratio
-        width={144} //Desried size with correct aspect ratio
-        alt="It's Joe"
-      />
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
+      <Layout>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <Image
+          src="/images/profile.jpg" // Route of the image file
+          height={144} //Desried size with correct aspect ratio
+          width={144} //Desried size with correct aspect ratio
+          alt="It's Joe"
+        />
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">
+            <a>Back to home</a>
+          </Link>
+        </h2>
+      </Layout>
     </>
   );
 }
